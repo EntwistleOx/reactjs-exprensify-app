2f6d5c16617d9c1fb9f4340789d7d09f1525ae82
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2019-09-10 00:26:06 -0300

    dispatch, subscribe and unsubscribe

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,18 @@ import { createStore } from 'redux'
 const store = createStore((state = { count: 0}, action) => {
     switch (action.type) {
         case 'INCREMENT': 
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             }
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
+            }
+        case 'SET':
+            return {
+                count: action.count
             }
         case 'RESET':
             return {
@@ -19,24 +25,39 @@ const store = createStore((state = { count: 0}, action) => {
     }
 })
 
-console.log(store.getState())
+// Subscribe - do someting when the store change
+// also return a function to unsubscribe
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState())
+})
 
 // Increment count
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 })
 
-console.log(store.getState())
+// unsubscribe()
+
+store.dispatch({
+    type: 'INCREMENT'
+})
 
 // Decrement count
 store.dispatch({
     type: 'RESET'
 })
 
-console.log(store.getState())
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+})
 
 store.dispatch({
     type: 'DECREMENT'
 })
 
-console.log(store.getState())
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
