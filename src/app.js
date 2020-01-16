af34bacc1ff7718931e21b1d039e19f4af3642c9
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-16 17:58:13 -0300

    import css datepicker

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense, editExpense, removeExpense } from './actions/expenses'
-import { setTextFilter, setStartDate, setEndDate } from './actions/filters'
+// import { addExpense, editExpense, removeExpense } from './actions/expenses'
+// import { setTextFilter, setStartDate, setEndDate } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
+import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 // store.subscribe(() => {
@@ -16,9 +17,14 @@ const store = configureStore()
 //     console.log(visibleExpenses)
 // })
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4500}))
-store.dispatch(addExpense({description: 'Gas bill', createdAt:1000}))
-store.dispatch(addExpense({description: 'Rent', amount: 109500}))
+// store.subscribe(() => {
+//     const state = store.getState()
+//     console.log(state)
+// })
+
+// store.dispatch(addExpense({description: 'Water bill', amount: 4500}))
+// store.dispatch(addExpense({description: 'Gas bill', createdAt:1000}))
+// store.dispatch(addExpense({description: 'Rent', amount: 109500}))
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
@@ -27,7 +33,7 @@ console.log(visibleExpenses)
 
 const jsx = (
     <Provider store={store}>
-    <AppRouter />
+        <AppRouter />
     </Provider>
 )
 
