24badd7584e9d1b1927b0cafa67e448caf6672d4
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-17 14:07:37 -0300

    section 12 complete

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,28 +3,61 @@ import { connect } from 'react-redux'
 import ExpenseForm from './ExpenseForm'
 import { editExpense, removeExpense } from '../actions/expenses'
 
-const EditExpensePage = (props) => {
-    return (
-        <div>
-            <h1>Edit Expense</h1>
-            <ExpenseForm 
-                expense={props.expense}
-                onSubmit={(expense) => {
-                    props.dispatch(editExpense(props.expense.id, expense))
-                    props.history.push('/')
-                }}
-            />
-           <button onClick={(e) => {
-               props.dispatch(removeExpense({ id: props.expense.id }))
-               props.history.push('/')
-            }}>Remove</button>
-        </div>
-    )
+export class EditExpensePage extends React.Component {
+    onSubmit = (expense) => {
+        this.props.editExpense(this.props.expense.id, expense)
+        this.props.history.push('/')
+    }
+
+    onRemove = (e) => {
+        this.props.removeExpense({ id: this.props.expense.id })
+        this.props.history.push('/')
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Edit Expense</h1>
+                <ExpenseForm
+                    expense={this.props.expense}
+                    onSubmit={this.onSubmit}
+                />
+                <button onClick={this.onRemove}>Remove</button>
+            </div>
+        )
+    }
 }
+
+// const EditExpensePage = (props) => {
+//     return (
+//         <div>
+//             <h1>Edit Expense</h1>
+//             <ExpenseForm 
+//                 expense={props.expense}
+//                 onSubmit={(expense) => {
+//                     props.dispatch(editExpense(props.expense.id, expense))
+//                     props.history.push('/')
+//                 }}
+//             />
+//            <button onClick={(e) => {
+//                props.dispatch(removeExpense({ id: props.expense.id }))
+//                props.history.push('/')
+//             }}>Remove</button>
+//         </div>
+//     )
+// }
 
 const mapStateToProps = (state, props) => {
     return {
         expense: state.expenses.find((expense) => expense.id === props.match.params.id)
     }
 }
-export default connect(mapStateToProps)(EditExpensePage)
+
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+        editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+        removeExpense: (data) => dispatch(removeExpense(data))
+    }
+} 
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage)
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -3,14 +3,14 @@ import { connect } from 'react-redux'
 import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters'
 import { DateRangePicker } from 'react-dates'
 
-class ExpenseListFilters extends React.Component{
+export class ExpenseListFilters extends React.Component {
     state = {
         calendarFocused: null
     }
 
     onDatesChange = ({ startDate, endDate }) => {
-        this.props.dispatch(setStartDate(startDate))
-        this.props.dispatch(setEndDate(endDate))
+        this.props.setStartDate(startDate)
+        this.props.setEndDate(endDate)
     }
 
     onFocusChange = (calendarFocused) => {
@@ -18,41 +18,45 @@ class ExpenseListFilters extends React.Component{
             calendarFocused
         }))
     }
-    
+
+    onTextChange = (e) => {
+        this.props.setTextFilter(e.target.value)
+    }
+
+    onSortChange = (e) => {
+        if (e.target.value === 'amount') {
+            this.props.sortByAmount()
+        } else if (e.target.value === 'date') {
+            this.props.sortByDate()
+        }
+    }
+
     render() {
         return (
             <div>
-            <input 
-                type="text" 
-                value={this.props.filters.text} 
-                onChange={(e) => {
-                    this.props.dispatch(setTextFilter(e.target.value))
-                }} 
-            />
-            <select 
-                value={this.props.filters.sortBy}
-                onChange={(e) => {
-                    if(e.target.value === 'amount'){
-                        this.props.dispatch(sortByAmount())
-                    }else if(e.target.value === 'date'){
-                        this.props.dispatch(sortByDate())
-                    }
-                }}
-            >
-                <option value="date">Date</option>
-                <option value="amount">Amount</option>
-            </select>
-            <DateRangePicker 
-                startDate={this.props.filters.startDate}
-                endDate={this.props.filters.endDate}
-                onDatesChange={this.onDatesChange}
-                focusedInput={this.state.calendarFocused}
-                onFocusChange={this.onFocusChange}
-                showClearDates={true}
-                numberOfMonths={1}
-                isOutsideRange={() => false}
-            />
-        </div>
+                <input
+                    type="text"
+                    value={this.props.filters.text}
+                    onChange={this.onTextChange}
+                />
+                <select
+                    value={this.props.filters.sortBy}
+                    onChange={this.onSortChange}
+                >
+                    <option value="date">Date</option>
+                    <option value="amount">Amount</option>
+                </select>
+                <DateRangePicker
+                    startDate={this.props.filters.startDate}
+                    endDate={this.props.filters.endDate}
+                    onDatesChange={this.onDatesChange}
+                    focusedInput={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    showClearDates={true}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                />
+            </div>
         )
     }
 }
@@ -62,4 +66,15 @@ const mapStateToProps = (state) => {
         filters: state.filters
     }
 }
-export default connect(mapStateToProps)(ExpenseListFilters)
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setTextFilter: (text) => dispatch(setTextFilter(text)),
+        sortByDate: () => dispatch(sortByDate()),
+        sortByAmount: () => dispatch(sortByAmount()),
+        setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+        setEndDate: (endDate) => dispatchEvent(setEndDate(endDate))
+
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters)
