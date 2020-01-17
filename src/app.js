621d3f00cebe54c057c553d31f94c9e92b7731f3
Juan Díaz <juan.diaz.12069@gmail.com>
2020-01-17 14:29:34 -0300

    section 13 Set Production Webpack

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
+
 // store.subscribe(() => {
 //     const state = store.getState()
 //     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
